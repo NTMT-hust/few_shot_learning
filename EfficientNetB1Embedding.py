6bb1b185d7a5b2bf5f585510cae20284df963599
Nguyen Tri <tri.ntm2400116@sis.hust.edu.vn>
2026-02-20 00:35:50 +0700

    initial commit

--- EfficientNetB1Embedding.py
+++ EfficientNetB1Embedding.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import timm
 
 class EfficientNetB1Embedding   (nn.Module):
-    def __init__(self, num_classes, pretrained=True, dropout_rate=0.3):
+    def __init__(self, pretrained=True):
         super(EfficientNetB1Embedding, self).__init__()
         # Load backbone
         self.backbone = timm.create_model('efficientnet_b1', pretrained=pretrained, num_classes = 0)
